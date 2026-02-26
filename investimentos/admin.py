730ba3349dd27ed87f7c1042ec317dfeca12bf8d
Manoel Silva <msjdevprojeto@gmail.com>
2026-02-25 22:46:31 -0300

    feat(admin): atualiza o admin.py com novas visualizações

--- investimentos/admin.py
+++ investimentos/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import TipoAtivo, Ativo, Lancamento
+from .models import Instituicao, Conta, TipoAtivo, Ativo, Lancamento, MovimentacaoConta
+
+@admin.register(Instituicao)
+class InstituicaoAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Conta)
+class ContaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'instituicao', 'tipo', 'saldo_inicial')
+    search_fields = ('nome', 'instituicao')
 
 @admin.register(TipoAtivo)
 class TipoAtivoAdmin(admin.ModelAdmin):
@@ -13,6 +23,12 @@ class AtivoAdmin(admin.ModelAdmin):
 
 @admin.register(Lancamento)
 class LancamentoAdmin(admin.ModelAdmin):
-    list_display = ('data', 'ativo', 'tipo_operacao', 'quantidade', 'preco_unitario', 'valor_total')
+    list_display = ('data', 'conta', 'ativo', 'tipo_operacao', 'quantidade', 'preco_unitario', 'valor_total')
     list_filter = ('tipo_operacao', 'ativo', 'data')
+    date_hierarchy = 'data'
+
+@admin.register(MovimentacaoConta)
+class MovimentacaoConta(admin.ModelAdmin):
+    list_display = ('data', 'conta', 'valor', 'descricao', 'tipo')
+    list_filter = ('conta', 'data')
     date_hierarchy = 'data'
